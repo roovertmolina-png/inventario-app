382d9de4eacdb5a15b77fabbec68767186dfd0f0
roovertmolina <roovertmolina@gmail.com>
2026-04-24 13:03:03 -0500

    fix db connection duplicate

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql2");
+
 const cors = require("cors");
 
 const app = express();
@@ -28,13 +28,6 @@ require('dotenv').config();
 
 console.log("PASSWORD:", process.env.DB_PASSWORD);
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
 
 db.connect(err => {
   if (err) {
