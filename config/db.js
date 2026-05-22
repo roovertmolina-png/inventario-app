e651680f7256dfa2c16a6b51203f5246c0a4c635
roovertmolina <roovertmolina@gmail.com>
2026-05-22 11:50:27 -0500

    Stabilize MySQL connection and add floor option

--- config/db.js
+++ config/db.js
@@ -1,21 +1,28 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
-  connectTimeout: 20000
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 20000,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.log("Error de conexión:", err);
-  } else {
-    console.log("Conectado a MySQL ✔");
+    console.log("Error de conexion:", err.message);
+    return;
   }
+
+  console.log("Conectado a MySQL");
+  connection.release();
 });
 
 module.exports = db;
